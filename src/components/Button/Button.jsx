84676b895dda2411ecc0f09e600f9da88a7d7898
Bia-source <bia_ferreirads@yahoo.com>
2024-03-20 17:21:42 -0300

    adicionando cards

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,15 @@
 
+// function somar(parametros)
+// somar(argumentos)
+// REACT -> function Button(props) 
+// PROPS -> Propriedades
+
 export default function Button({title, color, handleClick}){
    return (
     <button onClick={handleClick} style={{color}}>{title}</button>
    )
 }
+
 
 
 
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,33 +1,43 @@
 import { useState } from "react";
 import Button from "../../components/Button/Button";
+import "./style.css";
 
-export default function Home({title, color}) {
-   const [personagens, setPersonagens] = useState([]);
-   
-   async function handlePersonagens(){
+export default function Home({ title, color }) {
+    const [personagens, setPersonagens] = useState([]);
+
+    async function handlePersonagens() {
         await fetch("https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=265")
-        .then(res => res.json())
-        .then(setPersonagens)
-   }
+            .then(res => res.json())
+            .then(setPersonagens)
+    }
 
-   function showPersonagens(){
-    console.log(personagens)
-   }
+    function showPersonagens() {
+        console.log(personagens)
+    }
 
-   function deleteAll(){
-    setPersonagens([]);
-   }
+    function deleteAll() {
+        setPersonagens([]);
+    }
 
     return (
         <>
-         <Button title="Buscar" color="yellow" handleClick={handlePersonagens}/>
-         <Button title="Mostrar" color="green" handleClick={showPersonagens}/>
-         <Button title="Deletar" color="red" handleClick={deleteAll}/>
-         
-         {/* presentinho para voces pesquisarem e tentarem entender kk */}
-         {personagens.map(item => (
-            <h1>{item.name}</h1>
-         ))}
+            <button onClick={handlePersonagens}>mostrar</button>
+            <Button personagens={personagens} color="yellow" handleClick={handlePersonagens} />
+            <Button title="Mostrar" color="green" handleClick={showPersonagens} />
+            <Button title="Deletar" color="red" handleClick={deleteAll} />
+
+            {/* presentinho para voces pesquisarem e tentarem entender kk */}
+            {personagens.map(personagem => (
+                
+                <div key={personagem.id} className="card">
+                    <img src={personagem.image}/>
+                    <h1>{personagem.name}</h1>
+                    <div className="infos">
+                        <p className="firstInfo"> {personagem.gender} </p>
+                        <p className="secondInfo"> {personagem.hair} </p>
+                    </div>
+                </div>
+            ))}
         </>
     )
 }
